3be7f902ae38ce30f31aa9c4d5b2e2b91dbb943d
Emilio Coppa <ercoppa@gmail.com>
2014-04-07 20:55:08 +0200

    updating website

--- website/update.sh
+++ website/update.sh
@@ -3,13 +3,13 @@
 jekyll build
 
 git add ./*
-git commit -a -m "~"
+git commit -a -m "updating website"
 git push
 
 cd ../../HadoopDiagrams.gh-pages.git
 cp -a ../HadoopDiagrams/website/_site/* ./
 git add ./*
-git commit -a -m "~"
+git commit -a -m "updating website"
 git push
 
 exit 0
